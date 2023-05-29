d54d76e7df85eab329ece6224bb63e9ea414f23c
caiquedegaspari <caique.degaspare@gmail.com>
2023-05-29 08:53:09 -0300

    dictionary done

--- Dicitionary/index.js
+++ Dicitionary/index.js
@@ -2,34 +2,57 @@ function Dictionary() {
   var items = {}
 
   this.set = function(key, value) {
-
+    items[key] = value
   }
 
   this.delete = function(key) {
-    
+    if (this.has(key)) {
+      delete items[key]
+      return true
+    }
+    return false
   }
 
   this.has = function(key) {
-    
+    return items.hasOwnProperty(key)
   }
 
   this.get = function(key) {
-    
+    return this.has(key) ? items[key] : undefined
   }
 
   this.clear = function() {
-    
+     items = {}
   }
 
   this.size = function() {
-    
+    return Object.keys(items).length
   }
 
   this.keys = function() {
-    
+   return Object.keys(items) 
   }
 
   this.values = function() {
-    
+    var values = []
+    var keys = Object.keys(items)
+
+    for (var i = 0; i < keys.length; i++){
+      values.push(items[keys[i]])
+    }
+    return values
+  }
+
+  this.getItems = function() {
+    return items
   }
 }
+
+var dictionary = new Dictionary()
+
+dictionary.set('Rubrio', 'rubrio@email.com')
+dictionary.set('Dunis', 'dunis@email.com')
+dictionary.set('Dania', 'dania@email.com')
+
+
+console.log(dictionary.getItems())
