3ffbc1a4f9cf7681aa1faa620ebf0a474905fa46
caiquedegaspari <caique.degaspare@gmail.com>
2023-06-01 08:02:17 -0300

    start binary search tree

---
+++ tree/binaryTree.js
@@ -0,0 +1,41 @@
+function BinarySearchTree() {
+  var Node = function (key) {
+    this.key = key
+    this.left = null
+    this.right = null
+  } 
+  var root = null
+
+  this.insert = function (key) {
+
+  }
+
+  this.search = function (key) {
+
+  }
+
+  this.remove = function (key) {
+
+  }
+
+  this.min = function (key) {
+
+  }
+
+  this.max = function (key) {
+
+  }
+
+  this.inOrderTraverse = function (key) {
+
+  }
+
+  this.preOrderTraverse = function (key) {
+
+  }
+
+  this.postOrderTraverse = function (key) {
+
+  }
+
+}
