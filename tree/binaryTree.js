c7ada8805ce17dad4867dec74642502906b2026f
caiquedegaspari <caique.degaspare@gmail.com>
2023-06-02 18:54:32 -0300

    test remove from binary tree

--- tree/binaryTree.js
+++ tree/binaryTree.js
@@ -162,24 +162,31 @@ function printNode(value) {
 
 var tree = new BinarySearchTree()
 
-tree.insert(15)
-tree.insert(10)
-tree.insert(9)
-tree.insert(6)
-tree.insert(7)
-tree.insert(2)
-tree.insert(23)
-tree.insert(27)
-tree.insert(29)
-tree.insert(42)
 tree.insert(11)
+tree.insert(7)
+tree.insert(15)
+tree.insert(5)
+tree.insert(3)
+tree.insert(9)
+tree.insert(8)
+tree.insert(10)
 tree.insert(13)
-tree.insert(16)
+tree.insert(12)
+tree.insert(14)
+tree.insert(20)
 tree.insert(18)
-tree.insert(22)
+tree.insert(25)
+tree.insert(6)
 
 
-tree.preOrderTraverse(printNode)
+tree.inOrderTraverse(printNode)
+
+tree.remove(6)
+tree.remove(5)
+tree.remove(15)
+console.log('.')
+
+tree.inOrderTraverse(printNode)
 
 
 document.body.innerHTML = `
